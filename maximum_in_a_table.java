dc32d39ca05894b6701f31762c65b1bd121ff355
XITIJ <97301437+Kshitij-Vijay@users.noreply.github.com>
2022-06-22 22:10:51 +0530

    Add files via upload

---
+++ maximum_in_a_table.java
@@ -0,0 +1,53 @@
+/*
+
+<QUESTION> 509A - 18 :                          MAXIMUM IN A TABLE
+
+A. Maximum in Table
+time limit per test : 2 seconds
+memory limit per test : 256 megabytes
+
+An n × n table a is defined as follows:
+
+The first row and the first column contain ones, that is: ai, 1 = a1, i = 1 for all i = 1, 2, ..., n.
+Each of the remaining numbers in the table is equal to the sum of the number above it and the number to the left of it. In other words, the remaining elements are defined by the formula ai, j = ai - 1, j + ai, j - 1.
+These conditions define all the values in the table.
+
+You are given a number n. You need to determine the maximum value in the n × n table defined by the rules above.
+
+Input
+The only line of input contains a positive integer n (1 ≤ n ≤ 10) — the number of rows and columns of the table.
+
+Output
+Print a single line containing a positive integer m — the maximum value in the table.
+
+*/
+
+import java.util.Scanner;
+
+public class maximum_in_a_table {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = (Integer.parseInt(in.nextLine()));
+        int arr[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+                }
+            }
+        }
+        System.out.println(arr[n - 1][n - 1]);
+    }
+    /*
+     * PROGRAM WORKS WELL
+     * 
+     * Time : 171 ms
+     * Memory : 0KB
+     * Tests : 10
+     * https://codeforces.com/contest/509/submission/161493563
+     * 
+     */
+}
