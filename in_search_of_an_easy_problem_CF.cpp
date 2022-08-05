8b9f4582b727efc1eb0c2c97ba73fd58d3ba6f95
XITIJ <97301437+Kshitij-Vijay@users.noreply.github.com>
2022-08-05 15:21:56 +0530

    Add files via upload

---
+++ in_search_of_an_easy_problem_CF.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	while(n--){
+		int t;
+		cin>>t;
+		if(t==1){
+			cout<<"HARD"<<endl;
+			return 0;
+		}
+	}
+	cout<<"EASY"<<endl;
+}
